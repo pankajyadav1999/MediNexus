1415ff473e57dfb0e351e4de97d65a84f0d2da92
pankajyadav1999 <pankajyadav7607@gmail.com>
2025-08-31 13:25:29 +0530

    pankaj_dashborad_fix_31Aug25

--- src/app/modules/patients/patient-main/patient-main.ts
+++ src/app/modules/patients/patient-main/patient-main.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet,RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-patient-main',
   standalone:true,
-  imports: [RouterOutlet,RouterLink],
+  imports: [CommonModule,RouterModule],
   templateUrl: './patient-main.html',
   styleUrl: './patient-main.css'
 })
